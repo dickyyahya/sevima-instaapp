3c2917b131c4af81b1a172a3028fad36a7fc0b60
dickyyahya <dickyyahya25@gmail.com>
2026-08-01 08:25:02 +0700

    fixed before code migrasi

--- database/migrations/2026_08_01_011342_create_posts_table.php
+++ database/migrations/2026_08_01_011342_create_posts_table.php
@@ -1,11 +1,34 @@
-Schema::create('posts', function (Blueprint $table) {
-    $table->id();
+<?php
 
-    $table->foreignId('user_id')->constrained()->onDelete('cascade');
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-    $table->text('caption');
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+            Schema::create('posts', function (Blueprint $table) {
+            $table->id();
 
-    $table->string('image');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
-    $table->timestamps();
-});
+            $table->text('caption');
+
+            $table->string('image');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
